0553a958f812dddc019180bf46b5efd9af4bcefa
Alex <battle253@gmail.com>
2019-02-19 22:02:36 +0530

    first commit

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
+class GoogleAuth extends React.Component {
+	state = {
+		isSignedIn: null
+	};
+	componentDidMount() {
+		window.gapi.load("client: auth2", () => {
+			window.gapi.client
+				.init({
+					clientId:
+						"398439982924-so6841efng45vld34f6s4lgf3adkjfb8.apps.googleusercontent.com",
+					scope: "email"
+				})
+				.then(() => {
+					this.auth = window.gapi.auth2.getAuthInstance();
+					this.onAuthChange(this.auth.isSignedIn.get());
+					this.auth.isSignedIn.listen(this.onAuthChange);
+				});
+		});
+	}
+
+	onAuthChange = isSignedIn => {
+		if (isSignedIn) {
+			this.props.signIn(this.auth.currentUser.get().getId());
+		} else {
+			this.props.signOut();
+		}
+	};
+
+	renderAuthButton() {
+		if (this.props.isSignedIn === null) {
+			return null;
+		} else if (this.props.isSignedIn) {
+			return (
+				<button className="ui red google button" onClick={this.auth.signOut}>
+					<i className="google icon" />
+					Sign Out
+				</button>
+			);
+		} else {
+			return (
+				<button className="ui red google button" onClick={this.auth.signIn}>
+					<i className="google icon" />
+					Sign In with Google
+				</button>
+			);
+		}
+	}
+
+	render() {
+		return <div>{this.renderAuthButton()}</div>;
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		isSignedIn: state.auth.isSignedIn
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	{
+		signIn,
+		signOut
+	}
+)(GoogleAuth);
---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,34 @@
+import _ from "lodash";
+
+import * as actionTypes from "../actions/types";
+
+const reducer = (state = {}, action) => {
+	switch (action.type) {
+		case actionTypes.FETCH_STREAMS:
+			return {
+				...state,
+				..._.mapKeys(action.payload, "id")
+			};
+		case actionTypes.FETCH_STREAM:
+			return {
+				...state,
+				[action.payload.id]: action.payload
+			};
+		case actionTypes.CREATE_STREAM:
+			return {
+				...state,
+				[action.payload.id]: action.payload
+			};
+		case actionTypes.EDIT_STREAM:
+			return {
+				...state,
+				[action.payload.id]: action.payload
+			};
+		case actionTypes.DELETE_STREAM:
+			return _.omit(state, action.payload);
+		default:
+			return state;
+	}
+};
+
+export default reducer;
